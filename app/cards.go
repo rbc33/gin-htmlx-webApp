cc2724cf283c914fceb23b7a18c5f375ab1d5e81
rbc33 <ricardobenthem@gmail.com>
2025-06-09 18:16:59 +0200

    feat: Add card handling functionality

---
+++ app/cards.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"bytes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rbc33/gocms/common"
+	"github.com/rbc33/gocms/database"
+	"github.com/rbc33/gocms/views"
+	"github.com/rs/zerolog/log"
+)
+
+func cardHandler(c *gin.Context, database database.Database) ([]byte, error) {
+	var card_binding common.CardIdBinding
+	if err := c.ShouldBindUri(&card_binding); err != nil {
+		return nil, err
+	}
+
+	// Get the post with the ID
+	card, err := database.GetCard(card_binding.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate HTML page
+	post_view := views.MakeCardPage(card.JsonData)
+	html_buffer := bytes.NewBuffer(nil)
+	err = post_view.Render(c, html_buffer)
+	if err != nil {
+		log.Error().Msgf("%s", err)
+	}
+
+	return html_buffer.Bytes(), nil
+}

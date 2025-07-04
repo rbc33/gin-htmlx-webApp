a1fbc3edd50e5e4dca9f1386bc2288a8b6cda970
rbc33 <ricardobenthem@gmail.com>
2025-07-04 22:11:17 +0200

    favicon2

--- views/images_templ.go
+++ views/images_templ.go
@@ -239,7 +239,7 @@ func makeImages(images []common.Image) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 16, ");;\n\t\t        setupArrowNavigation()\n\n      });\n    </script>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 16, ")\n\t\t        setupArrowNavigation()\n\n      });\n    </script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}

b9a60cae0a9500bb5a999f0bfda822047c5916bf
rbc33 <ricardobenthem@gmail.com>
2025-07-07 23:20:07 +0200

    cors

--- admin-app/app.go
+++ admin-app/app.go
@@ -93,7 +93,6 @@ func SetupRoutes(settings common.AppSettings, shortcode_handlers map[string]*lua
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 

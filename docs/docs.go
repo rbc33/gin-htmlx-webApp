6067b5c40c4726164a8acf3c02e7251d31759eea
rbc33 <ricardobenthem@gmail.com>
2025-07-15 16:47:44 +0200

    make both domains to work, didnt work

--- docs/docs.go
+++ docs/docs.go
@@ -1467,9 +1467,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "localhost:8081",
-	BasePath:         "/",
-	Schemes:          []string{"http"},
+	Host:             "",
+	BasePath:         "",
+	Schemes:          []string{},
 	Title:            "GoCMS Admin API",
 	Description:      "This is the admin API for the GoCMS app.",
 	InfoInstanceName: "swagger",
